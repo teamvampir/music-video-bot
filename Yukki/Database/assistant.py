2fb311135b297fb12cc994a56c9860fd5e3efeec
Logesh Kumar <130851416+logesh-works@users.noreply.github.com>
2023-09-08 01:44:24 +0530

    Update assistant.py

--- Yukki/Database/assistant.py
+++ Yukki/Database/assistant.py
@@ -5,13 +5,18 @@ from Yukki import db
 assisdb = db.assistantmultimode
 
 
-async def save_assistant(chat_id: int, name: str, note: dict):
-    name = name.lower().strip()
-    _notes = await _get_assistant(chat_id)
-    _notes[name] = note
-    await assisdb.update_one(
-        {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
-    ) 
+async def get_as_names(chat_id: int) -> List[str]:
+    _notes = []
+    for note in await _get_assistant(chat_id):
+        _notes.append(note)
+    return _notes
+
+
+async def _get_assistant(chat_id: int) -> Dict[str, int]:
+    _notes = await assisdb.find_one({"chat_id": chat_id})
+    if not _notes:
+        return {}
+    return _notes["notes"]
 
 
 async def get_assistant(chat_id: int, name: str) -> Union[bool, dict]:
@@ -25,18 +30,8 @@ async def get_assistant(chat_id: int, name: str) -> Union[bool, dict]:
 
 async def save_assistant(chat_id: int, name: str, note: dict):
     name = name.lower().strip()
-    
-    # Serialize the note object to a dictionary
-    note_dict = {
-        "field1": note.field1,  # Replace with actual field names from the note object
-        "field2": note.field2,
-        # Add more fields as needed
-    }
-    
     _notes = await _get_assistant(chat_id)
-    _notes[name] = note_dict  # Store the serialized note
-    
+    _notes[name] = note
     await assisdb.update_one(
         {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
     )
-
