3915ff41a0ff291e918aea34924e3c2633b6d0e1
Logesh Kumar <130851416+logesh-works@users.noreply.github.com>
2023-09-08 01:29:57 +0530

    Update assistant.py

--- Yukki/Database/assistant.py
+++ Yukki/Database/assistant.py
@@ -30,8 +30,18 @@ async def get_assistant(chat_id: int, name: str) -> Union[bool, dict]:
 
 async def save_assistant(chat_id: int, name: str, note: dict):
     name = name.lower().strip()
+    
+    # Serialize the note object to a dictionary
+    note_dict = {
+        "field1": note.field1,  # Replace with actual field names from the note object
+        "field2": note.field2,
+        # Add more fields as needed
+    }
+    
     _notes = await _get_assistant(chat_id)
-    _notes[name] = note
+    _notes[name] = note_dict  # Store the serialized note
+    
     await assisdb.update_one(
         {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
     )
+
